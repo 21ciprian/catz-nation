976a4d5da8462cbec944bd3a762dcd75cf9562bb
21ciprian <2021ciprianoffice@gmail.com>
2022-07-21 01:53:15 +0100

    Added animation to burger menu on mobile screen and hide the component on desktop screen

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -4,6 +4,9 @@ interface IProps {
 	menuOpen: boolean
 	setMenuOpen: (arg: boolean) => void
 }
+interface ISpan {
+	open?: any
+}
 const Wrapper = styled.div`
 	width: 2rem;
 	height: 1.5rem;
@@ -11,8 +14,11 @@ const Wrapper = styled.div`
 	flex-direction: column;
 	justify-content: space-between;
 	cursor: pointer;
+	@media (min-width: 1080px) {
+		display: none;
+	}
 `
-const Span = styled.span`
+const Span = styled.span<ISpan>`
 	width: 100%;
 	height: 0.18rem;
 	background-color: #000;
@@ -23,21 +29,21 @@ const Span = styled.span`
 	transform-origin: left;
 	transition: all 1s ease;
 	&:nth-of-type(2) {
-		/* opacity: ${props => (props ? 0 : 1)}; */
+		opacity: ${(props: any) => (props.open ? 0 : 1)};
 	}
 	&:first-of-type {
-		/* transform: rotate(${props => (props ? '42deg' : '')}); */
+		transform: rotate(${(props: any) => (props.open ? '42deg' : '')});
 	}
 	&:last-of-type {
-		/* transform: rotate(${props => (props ? '-42deg' : '')}); */
+		transform: rotate(${(props: any) => (props.open ? '-42deg' : '')});
 	}
 `
 const Hamburger = ({menuOpen, setMenuOpen}: IProps) => {
 	return (
 		<Wrapper onClick={() => setMenuOpen(!menuOpen)}>
-			<Span></Span>
-			<Span></Span>
-			<Span></Span>
+			<Span open={menuOpen}></Span>
+			<Span open={menuOpen}></Span>
+			<Span open={menuOpen}></Span>
 		</Wrapper>
 	)
 }
